87de5dd0b6eb65bf695270b6342c838814ff0c7e
Srikavya Vasala <95865936+srikavya26@users.noreply.github.com>
2023-12-09 21:51:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -122,26 +122,5 @@ function scrolling() {
             img.style.animationPlayState = "running"
         }
     })
-}
-document.addEventListener("DOMContentLoaded", function () {
-  const images = document.querySelectorAll(".photo-grid img");
+};
 
-  const options = {
-    threshold: 0.5, // Adjust this threshold as needed
-  };
-
-  const imageObserver = new IntersectionObserver(function (entries, observer) {
-    entries.forEach(function (entry) {
-      if (entry.isIntersecting) {
-        const lazyImage = entry.target;
-        lazyImage.src = lazyImage.dataset.src;
-        lazyImage.classList.remove("lazy");
-        imageObserver.unobserve(lazyImage);
-      }
-    });
-  }, options);
-
-  images.forEach(function (image) {
-    imageObserver.observe(image);
-  });
-});
